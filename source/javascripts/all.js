1871067a93922f68e6d05a8c5f1ed546012b0eec
Thebeachmaster <otienoken@outlook.com>
2017-11-23 12:36:23 +0300

    Merged Changes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-//= require ./lib/_energize
-//= require ./app/_lang
-//= require ./app/_search
-//= require ./app/_toc
-=======
 //= require ./all_nosearch
 //= require ./app/_search
->>>>>>> upstream/master
--- source/javascripts/all_nosearch.js
+++ source/javascripts/all_nosearch.js
@@ -1,22 +1,16 @@
-<<<<<<< HEAD
-//= require ./lib/_energize
-//= require ./app/_lang
-//= require ./app/_toc
-=======
 //= require ./lib/_energize
 //= require ./app/_toc
 //= require ./app/_lang
 
 $(function() {
-  loadToc($('#toc'), '.toc-link', '.toc-list-h2', 10);
-  setupLanguages($('body').data('languages'));
-  $('.content').imagesLoaded( function() {
-    window.recacheHeights();
-    window.refreshToc();
-  });
+    loadToc($('#toc'), '.toc-link', '.toc-list-h2', 10);
+    setupLanguages($('body').data('languages'));
+    $('.content').imagesLoaded(function() {
+        window.recacheHeights();
+        window.refreshToc();
+    });
 });
 
 window.onpopstate = function() {
-  activateLanguage(getLanguageFromQueryString());
+    activateLanguage(getLanguageFromQueryString());
 };
->>>>>>> upstream/master
